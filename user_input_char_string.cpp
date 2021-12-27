6e2f33b66be46729a85e27b1c0b25a9af779e789
MD Omar Jilani Jidan <66664781+jidan0777@users.noreply.github.com>
2021-12-27 22:05:29 +0600

    Add files via upload

---
+++ user_input_char_string.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    char str[100];
+    cin.get(str,100);
+    cout<<"Hello, World."<<endl;
+    cout<<str<<endl;
+    getch();
+}
